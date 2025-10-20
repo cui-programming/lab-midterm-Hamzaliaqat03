ba6e2021c2b7e44b971b04a3ab0d8ebf3790059d
Hamzaliaqat03 <130701462+Hamzaliaqat03@users.noreply.github.com>
2025-10-20 13:07:51 +0500

    Update student.js

--- src/config/student.js
+++ src/config/student.js
@@ -1,4 +1,4 @@
 // Edit these two fields with your details before you start.
 // They will be shown at the very top of the screen.
-export const STUDENT_NAME = "Your Name";
-export const REG_NO = "REG-000000";
+export const STUDENT_NAME = "hamza liaqat";
+export const REG_NO = "sp23-bse-015";
