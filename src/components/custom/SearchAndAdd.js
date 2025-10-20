250561ef0a957f3f260a4c453f7cb59cc2e51336
Hamzaliaqat03 <130701462+Hamzaliaqat03@users.noreply.github.com>
2025-10-20 13:09:19 +0500

    Update SearchAndAdd.js

--- src/components/custom/SearchAndAdd.js
+++ src/components/custom/SearchAndAdd.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import { styles } from '../../styles/styles';
+import { TextInput, Button } from '../ui';
 
 /**
  * Custom/SearchAndAdd
@@ -11,8 +12,21 @@ import { styles } from '../../styles/styles';
  *  - lifting state up if needed
  */
 export default function SearchAndAdd() {
+
+
+  
+
+
+
   return (
     <View style={styles.section}>
+      
+      <TextInput placeholder="Search Azkaar" style={styles.input} />
+      <Button children="Search" onPress={() => {}} />
+
+
+      <TextInput placeholder="New Zikr Phrase" style={styles.input} />
+      <Button children="Add Zikr" onPress={() => {}} />
       {/* TODO: Implement search and add UI here using ui/TextInput and ui/Button */}
     </View>
   );
