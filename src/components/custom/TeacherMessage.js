e8847cca300fdd569b673e54abd248260f992297
Hamzaliaqat03 <130701462+Hamzaliaqat03@users.noreply.github.com>
2025-10-20 13:08:55 +0500

    Update TeacherMessage.js

--- src/components/custom/TeacherMessage.js
+++ src/components/custom/TeacherMessage.js
@@ -1,1 +1,17 @@
 
+import React from 'react';
+import { View, Text } from 'react-native'; // TODO: replace Text with ui/Text after export
+import { styles } from '../../styles/styles';
+
+/**
+ * Custom/AboutMe
+ * Shows name and registration number at the top.
+ * Props: name (string), regNo (string)
+ */
+export default function TeacherMessage() {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerText}>this is teacher message</Text>
+    </View>
+  );
+}
